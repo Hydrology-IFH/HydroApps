e5267c7e9af8b151b6b1839c7858145ee9402140
maxschmi <maxschm@yahoo.de>
2023-02-21 09:41:34 +0100

    create Model and home page to create home based on model

---
+++ HydroApps/models.py
@@ -0,0 +1,28 @@
+from django.contrib.gis.db import models
+
+
+class HydroApps(models.Model):
+    name = models.CharField(
+        max_length=20, 
+        unique=True, 
+        primary_key=True, 
+        help_text="The URL to the welcome page of this app.")
+    order = models.IntegerField(
+        help_text="A number to set the ordering on the front page.")
+    short_explanation_en = models.CharField(
+        max_length=500, help_text="A short explanation of this app in english.")
+    short_explanation_de = models.CharField(
+        max_length=500, help_text="A short explanation of this app in german.")
+    home_url = models.CharField(
+        max_length=100, 
+        help_text="The django URL-dispatcher-name or real URL to the welcome page of this app. E.g. 'weatherDB:home'")
+    logo_url = models.CharField(
+        max_length=100, 
+        help_text="The django static URL-name where to find the logo of this App. E.g. 'weatherDB/logo.png'")
+    is_released = models.BooleanField(
+        help_text="Should this app already be shown to the public?")
+    is_external = models.BooleanField(
+        help_text="Is this app hosted externally?")
+    
+    class Meta:
+        ordering=["order", "name"]
--- HydroApps/views.py
+++ HydroApps/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render
 from main.utils.utils import get_context_extra
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from .models import HydroApps
 
 def home_view(request, *args, **kwargs): 
-    return render(request, "HydroApps/home.html", get_context_extra(request, **kwargs))
+    context = get_context_extra(request, **kwargs)
+    if context["show_unreleased"]:
+        context.update({"apps": HydroApps.objects.all()})
+    else:
+        context.update({"apps": HydroApps.objects.get(is_released=True)})
+    return render(request, "HydroApps/home.html", context)
 
 def redirect_to_base(request):
     # to remove the HydroApps part from an URL
