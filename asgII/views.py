3db0104d3268f4bc09122e0cd1b9b2d7e3983b8d
maxschmi <maxschm@yahoo.de>
2023-04-04 14:08:01 +0200

    release app asg-II

--- asgII/views.py
+++ asgII/views.py
@@ -6,15 +6,12 @@ from django.core.serializers import serialize
 from main.settings import DEBUG
 from pathlib import Path
 from main.utils.utils import get_context_base
-from main.decorators import unreleased
 
 # Create your views here.
-@unreleased
 def home_view(request, *args, **kwargs):
     context = get_context_base(request)
     return render(request, "asgII/home.html", context)
 
-@unreleased
 def map_view(request, *args, **kwargs):
     context = get_context_base(request)
     context.update({
@@ -22,12 +19,10 @@ def map_view(request, *args, **kwargs):
         })
     return render(request, "asgII/asgII_map.html", context)
 
-@unreleased
 def method_view(request, *args, **kwargs):
     context = get_context_base(request)
     return render(request, "asgII/method.html", context)
 
-@unreleased
 def package_view(request, *args, **kwargs):
     context = get_context_base(request)
     return render(request, "asgII/package.html", context)
