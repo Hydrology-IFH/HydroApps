aa7f44bbd2af9e4b4e08d70d41f15dc547c35aae
maxschmi <maxschm@yahoo.de>
2023-06-01 17:12:34 +0200

    add Measurement Model

--- HydroApps/models.py
+++ HydroApps/models.py
@@ -27,3 +27,19 @@ class HydroApps(models.Model):
     
     class Meta:
         ordering=["order", "name"]
+
+class HydroLinks(models.Model):
+    name = models.CharField(
+        max_length=20, 
+        unique=True, 
+        primary_key=True, 
+        help_text="The URL to the welcome page of this app.")
+    order = models.IntegerField(
+        default=1,
+        help_text="A number to set the ordering on the front page.")
+    url = models.CharField(
+        max_length=100, 
+        help_text="The URL to this measurement Application")
+    
+    class Meta:
+        ordering=["order", "name"]
