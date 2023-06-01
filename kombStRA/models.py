d6cfd7c285798a9a468667be0e52940766534715
maxschmi <maxschm@yahoo.de>
2023-06-01 16:43:28 +0200

    add basic structure

--- kombStRA/models.py
+++ kombStRA/models.py
@@ -1,7 +1,6 @@
 from django.contrib.gis.db import models
-from django.db.models.base import ModelBase
 
-class Grid(models.Model):
+class KombStRAGrid(models.Model):
     grid_id = models.IntegerField(unique=True, primary_key=True)
     geometry = models.PolygonField(srid=4326, blank=True, null=True)
 
