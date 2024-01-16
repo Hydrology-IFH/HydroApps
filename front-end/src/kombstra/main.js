e9b6bd62984ab27fe90f10cf575fc953db86e4bb
maxschmi <maxschm@yahoo.de>
2024-01-16 14:53:03 +0100

    add legend to kombstra put radolan_layer to module update styles of SRI and Month to be categorical

--- front-end/src/kombstra/main.js
+++ front-end/src/kombstra/main.js
@@ -4,67 +4,15 @@ import "ol-ext/control/Legend.css";
 
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/WebGLTile.js';
-import proj4 from 'proj4';
-import {get as getProjection} from 'ol/proj.js';
-import { register } from 'ol/proj/proj4.js';
-import GeoTIFF from 'ol/source/GeoTIFF.js';
+
 import OSM from 'ol/source/OSM';
-import get_style from './styles.js';
-import { form, formPara, formPerc, formRank } from './form.js';
-// import Legend as LegendCont from "ol/control";
-// import Legend from "ol/legend";
-import Legend from "ol-ext/legend/Legend";
-import LegendCtrl from "ol-ext/control/Legend";
-
-let debug = false;
-
-// found on https://www.spatialreference.org/ref/sr-org/7019/
-// and adjusted for proj4js: https://github.com/proj4js/proj4js/issues/456
-proj4.defs("SR-ORG:97019", '+proj=stere +lat_0=90 +lat_ts=60 +lon_0=10 +k=1 +x_0=0 +y_0=0 +a=6370040 +b=6370040 +to_meter=1000 +units=km +no_defs');
-register(proj4);
-const proj_radolan = getProjection("SR-ORG:97019");
-
-
-let get_tif_source = function(){
-  // get tiff url
-  let para = formPara.value;
-  let perc = formPerc.value;
-  let rank = formRank.value;
-  let static_stem = "/static";
-  let tif_url = `${static_stem}/kombstra/kombstra_views/${perc}_${rank}_${para}.tif`;
-  let tif_blob;
-  if (debug){
-    tif_blob = fetch(tif_url).then(response => response.blob());
-  }
-
-  // create source
-  let source = new GeoTIFF({
-    sources: [
-      {
-        bands: [1],
-        ...(debug? {blob:tif_blob}:{url:tif_url})
-      }
-    ],
-    sourceOptions: {
-      allowFullFile: true,
-    },
-    interpolate: false,
-    normalize: false,
-  })
-  source.projection = proj_radolan;
-  return source;
-}
-let tif_source = get_tif_source();
-const init_tif_view = await tif_source.getView();
-
-// create layer
-let radolan_layer = new TileLayer({
-  source: tif_source,
-  zIndex: 2,
-  style: get_style(),
-});
+import { get_style } from './modules/styles.js';
+import { form } from './modules/form.js';
+import { create_legend, update_legend} from './modules/legend.js';
+import { get_tif_source, radolan_layer } from './modules/radolan_layer.js';
 
 // create the map
+const init_tif_view = await radolan_layer.getSource().getView();
 const map = new Map({
   target: 'map',
   layers: [
@@ -85,8 +33,10 @@ const map = new Map({
 
 // update function
 function updateLayer() {
+  let style = get_style();
   radolan_layer.setSource(get_tif_source());
-  radolan_layer.setStyle(get_style());
+  radolan_layer.setStyle(style);
+  update_legend();
 }
 form.addEventListener(["submit"], updateLayer);
 
@@ -96,38 +46,10 @@ function displayPixelValue(event) {
 }
 map.on(['click'], displayPixelValue);
 
-// legend
-var legend = new Legend({
-  title: 'Legend',
-  margin: -5,
-  maxWidth: 300,
-});
-
-// Set the style of the legend
-legend.setStyle({
-  color: [
-    'interpolate',
-    ['linear'],
-    ["band", 1],
-    10, [0, 0, 0],
-    360, [255, 255, 255],
-  ]
-});
-
-// Set the layer of the legend
-legend.setLayer(radolan_layer);
-
-const legendCtrl = new LegendCtrl({
-  legend: legend,
-  collapsed: false
-});
-
-// Add the Legend control to the map
-map.addControl(legendCtrl);
+// create legend
+create_legend(map);
 
 window.map = map;
 window.form = form;
 window.radolan_layer = radolan_layer;
-// window.legendCtrl = legendCtrl;
-window.legend = legend;
 window.get_style = get_style;
