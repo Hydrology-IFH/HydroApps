b26cad71607d53b3da3af2e340622c8cf141e25d
maxschmi <maxschm@yahoo.de>
2024-01-30 16:13:04 +0100

    only show nesesarry fields in API

--- kombstra/serializers.py
+++ kombstra/serializers.py
@@ -6,9 +6,9 @@ from .models import KombStRAData, KombStRAPolygons
 class KombStRADataSerializer(serializers.ModelSerializer):
     class Meta:
         model = KombStRAData
-        fields = '__all__'  # replace this with the fields you want to expose
+        fields = ["event_rank", "date", "duration", "pval", "pint", "sri"]
 
 class KombStRAPolygonsSerializer(serializers.ModelSerializer):
     class Meta:
         model = KombStRAPolygons
-        fields = '__all__'  # replace this with the fields you want to expose
+        fields = '__all__'
