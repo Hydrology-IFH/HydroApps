583f41dc347150842d6bb2bf66904c9172763bf9
maxschmi <maxschm@yahoo.de>
2024-02-06 11:40:29 +0100

    only run i18n-po-to-js when po exists

--- front-end/i18next-scanner.config.cjs
+++ front-end/i18next-scanner.config.cjs
@@ -1,12 +1,11 @@
 module.exports = {
-    output: 'src/kombstra',
     input: ['src/kombstra/**/*.{js,vue}'],
     options: {
         lngs: ['de'],
         defaultLng: 'en',
         resource: {
-            loadPath: 'locales/{{lng}}/{{ns}}.json',
-            savePath: 'locales/{{lng}}/{{ns}}.json',
+            loadPath: 'src/kombstra/locales/{{lng}}/{{ns}}.json',
+            savePath: 'src/kombstra/locales/{{lng}}/{{ns}}.json',
             jsonIndent: 2,
             lineEnding: '\n'
         },
@@ -14,15 +13,7 @@ module.exports = {
             list: ['i18next.t', 'i18n.t', 'this.$t', '$t'],
             extensions: ['.js', '.vue']
         },
-        interpolation: {
-            prefix: '{{',
-            suffix: '}}',
-        },
-        trans: {
-            component: 'Trans',
-            extensions: [],
-        },
-        debug: true,
+        debug: false,
         pluralSeparator: 'xacy_never-happens-ß?876',
         nsSeparator: false, // namespace separator
         keySeparator: false, // key separator
