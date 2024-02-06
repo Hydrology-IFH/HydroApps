efa32e64b4928bbd00513df8b6e4ab89a205ad3e
maxschmi <maxschm@yahoo.de>
2024-02-06 12:00:03 +0100

    reformat top same size

--- front-end/src/kombstra/modules/PopupContent.vue
+++ front-end/src/kombstra/modules/PopupContent.vue
@@ -1,131 +1,132 @@
 <script>
 export default {
-    data: function () {
-        return {
-            cell_data: [],
-            grid_id: null,
-            loading: true,
-            error_msg: false,
-            cell_lat: null,
-            cell_lon: null
-        }
-    },
-    watch: {
-        grid_id(new_grid_id, old_grid_id) {
-            if (((new_grid_id !== old_grid_id) && (new_grid_id !== undefined)) || (this.error_msg) || (this.cell_data.length != 0)) {
-                this.fetchData();
-            }
-        }
-    },
-    methods: {
-        update_popup_data(grid_id, lat, long) {
-            this.cell_lat = lat;
-            this.cell_lon = long;
-            this.grid_id = grid_id;
-        },
-        async fetchData() {
-            this.loading = true;
-            fetch("/en/kombstra/api/kombstra_data/?grid_id=" + this.grid_id)
-                .then((res) => res.json())
-                .then((data) => {
-                    this.cell_data = data;
-                    this.loading = false;})
-                .catch((err) => {
-                    this.set_error_msg("We are sorry, there was a problem fetching the data for this cell.");
-                    console.log(err);
-                });
-        },
-        set_error_msg(msg) {
-            this.loading = false;
-            this.error_msg = msg;
-        },
-        download_data() {
-            // create the csv content
-            let header = [
-                `This is the KombStRA data for the raster cell with the ID ${this.grid_id}`,
-                `The cells center is located at ${this.cell_lat}, ${this.cell_lon}.`,
-                "The columns are:",
-                "event_rank : The ranking of the event",
-                "date: The date of the event",
-                "duration: The duration of the event in minutes",
-                "pval: The rain amount in mm",
-                "pint: The rain intensity in mm/h",
-                "sri: The 'Starkregenindex' (SRI) of the event",
-                ""
-            ];
-            let items = this.cell_data;
-            let col_names = Object.keys(items[0]);
-            let csv = [
-                ...header,
-                col_names.join(','),
-                ...items.map(row => {
-                    return col_names.map(fieldName => {
-                        return JSON.stringify(row[fieldName])
-                    }).join(',')
-                })
-            ].join('\r\n')
-
-            // create a hidden element and download the csv file
-            let hiddenElement = document.createElement('a');
-            hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
-            hiddenElement.target = '_blank';
-            hiddenElement.download = `kombstra_data_${this.grid_id}.csv`;
-            hiddenElement.click();
-        }
+  data: function () {
+    return {
+      cell_data: [],
+      grid_id: null,
+      loading: true,
+      error_msg: false,
+      cell_lat: null,
+      cell_lon: null
     }
+  },
+  watch: {
+    grid_id(new_grid_id, old_grid_id) {
+      if (((new_grid_id !== old_grid_id) && (new_grid_id !== undefined)) || (this.error_msg) || (this.cell_data.length != 0)) {
+        this.fetchData();
+      }
+    }
+  },
+  methods: {
+    update_popup_data(grid_id, lat, long) {
+      this.cell_lat = lat;
+      this.cell_lon = long;
+      this.grid_id = grid_id;
+    },
+    async fetchData() {
+      this.loading = true;
+      fetch("/en/kombstra/api/kombstra_data/?grid_id=" + this.grid_id)
+        .then((res) => res.json())
+        .then((data) => {
+          this.cell_data = data;
+          this.loading = false;
+        })
+        .catch((err) => {
+          this.set_error_msg("We are sorry, there was a problem fetching the data for this cell.");
+          console.log(err);
+        });
+    },
+    set_error_msg(msg) {
+      this.loading = false;
+      this.error_msg = msg;
+    },
+    download_data() {
+      // create the csv content
+      let header = [
+        `This is the KombStRA data for the raster cell with the ID ${this.grid_id}`,
+        `The cells center is located at ${this.cell_lat}, ${this.cell_lon}.`,
+        "The columns are:",
+        "event_rank : The ranking of the event",
+        "date: The date of the event",
+        "duration: The duration of the event in minutes",
+        "pval: The rain amount in mm",
+        "pint: The rain intensity in mm/h",
+        "sri: The 'Starkregenindex' (SRI) of the event",
+        ""
+      ];
+      let items = this.cell_data;
+      let col_names = Object.keys(items[0]);
+      let csv = [
+        ...header,
+        col_names.join(','),
+        ...items.map(row => {
+          return col_names.map(fieldName => {
+            return JSON.stringify(row[fieldName])
+          }).join(',')
+        })
+      ].join('\r\n')
+
+      // create a hidden element and download the csv file
+      let hiddenElement = document.createElement('a');
+      hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+      hiddenElement.target = '_blank';
+      hiddenElement.download = `kombstra_data_${this.grid_id}.csv`;
+      hiddenElement.click();
+    }
+  }
 }
 </script>
 
 <template>
-    <div class="spinner-border text-primary m-2" role="status" v-if="loading">
-        <span class="sr-only"></span>
-    </div>
-    <div v-else-if="error_msg" class="alert alert-danger m-2" role="alert" style="max-width:300px;">
-        <h4 class="alert-heading"><i class="bi bi-exclamation-triangle"></i> Error</h4>
-        <p>{{ error_msg }}</p>
-        <p>Please try again later.</p>
-    </div>
-    <div v-else class="popup-data">
+  <div class="spinner-border text-primary m-2" role="status" v-if="loading">
+    <span class="sr-only"></span>
+  </div>
+  <div v-else-if="error_msg" class="alert alert-danger m-2" role="alert" style="max-width:300px;">
+    <h4 class="alert-heading"><i class="bi bi-exclamation-triangle"></i> Error</h4>
+    <p>{{ error_msg }}</p>
+    <p>Please try again later.</p>
+  </div>
+  <div v-else class="popup-data">
 
-        <div class="popup-header">
-            <p>These are the events for this cell that got categorized.</p>
-            <button class="btn btn-primary"
-                data-bs-toggle="tooltip" data-bs-placement="top" data-bs-container="body"
-                data-bs-title='Download the data for this cell as csv.'><i class="bi bi-download" @click="download_data"></i></button>
-        </div>
-        <div class="tab-content">
-            <table class="table table-striped table-hover">
-                <thead class="table-light ">
-                    <tr>
-                        <th scope="col">Event rank</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Duration</th>
-                        <th scope="col">Rain amount</th>
-                        <th scope="col">Rain intensity</th>
-                        <th scope="col">SRI</th>
-                    </tr>
-                    <tr class="th-units">
-                        <th scope="col"></th>
-                        <th scope="col"></th>
-                        <th scope="col">min</th>
-                        <th scope="col">mm</th>
-                        <th scope="col">mm/h</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="data in cell_data" :key="data.data_id">
-                        <td>{{ data.event_rank }}</td>
-                        <td>{{ data.date }}</td>
-                        <td>{{ data.duration }}</td>
-                        <td>{{ data.pval }}</td>
-                        <td>{{ data.pint }}</td>
-                        <td>{{ data.sri }}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+    <div class="popup-header">
+      <p>These are the events for this cell that got categorized.</p>
+      <button class="btn btn-primary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-container="body"
+        data-bs-title='Download the data for this cell as csv.'><i class="bi bi-download"
+          @click="download_data"></i></button>
     </div>
+    <div class="tab-content">
+      <table class="table table-striped table-hover">
+        <thead class="table-light ">
+          <tr>
+            <th scope="col">Event rank</th>
+            <th scope="col">Date</th>
+            <th scope="col">Duration</th>
+            <th scope="col">Rain amount</th>
+            <th scope="col">Rain intensity</th>
+            <th scope="col">SRI</th>
+          </tr>
+          <tr class="th-units">
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col">min</th>
+            <th scope="col">mm</th>
+            <th scope="col">mm/h</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="data in cell_data" :key="data.data_id">
+            <td>{{ data.event_rank }}</td>
+            <td>{{ data.date }}</td>
+            <td>{{ data.duration }}</td>
+            <td>{{ data.pval }}</td>
+            <td>{{ data.pint }}</td>
+            <td>{{ data.sri }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 </template>
 
 
--- front-end/src/kombstra/modules/popup.js
+++ front-end/src/kombstra/modules/popup.js
@@ -20,24 +20,24 @@ const closer = document.getElementById('popup-closer');
  * Create an overlay to anchor the popup to the map.
  */
 const overlay = new Overlay({
-    element: container,
-    autoPan: {
+  element: container,
+  autoPan: {
     animation: {
-        duration: 250,
-        },
+      duration: 250,
     },
-    className: 'ol-overlay-popup',
+  },
+  className: 'ol-overlay-popup',
 });
 
 const popup_cell_source = new VectorSource({});
 const popup_cell_layer = new VectorLayer({
-    source: popup_cell_source,
-    map: map,
-    style: {
-        'stroke-color': 'rgba(225, 0, 255, 1)',
-        'stroke-width': 4,
-    },
-    zIndex: 9,
+  source: popup_cell_source,
+  map: map,
+  style: {
+    'stroke-color': 'rgba(225, 0, 255, 1)',
+    'stroke-width': 4,
+  },
+  zIndex: 9,
 });
 
 // create PopupContent component
@@ -46,83 +46,83 @@ let popupAppInst = popupApp.mount(content);
 
 // functions to set popup position
 function set_overlay_position() {
-    let ext = popup_cell_source.getExtent();
-    overlay.setPosition([(ext[0] + ext[2]) / 2, ext[3]]);
+  let ext = popup_cell_source.getExtent();
+  overlay.setPosition([(ext[0] + ext[2]) / 2, ext[3]]);
 };
 function set_overlay_position_error(long, lat) {
-    overlay.setPosition([long, lat]);
+  overlay.setPosition([long, lat]);
 };
 
 // get kombstra data from api
 function update_kombstra_data(long, lat) {
-    fetch("/en/kombstra/api/kombstra_polygon/?long=" + long + "&lat=" + lat)
-        .then((res) => res.json())
-        .then((data) => {
-            popup_cell_source.addFeatures(
-                new GeoJSON().readFeatures(data[0].geometry, {
-                    dataProjection: "SR-ORG:97019",
-                    featureProjection: "SR-ORG:97019",
-                }));
-            popupAppInst.update_popup_data(data[0].grid_id, lat, long);
-            set_overlay_position();
-        })
-        .catch((err) => {
-            console.log(err);
-            popupAppInst.set_error_msg("We are sorry, there was an error finding the data for this location.");
-            set_overlay_position_error(long, lat);
-        });
+  fetch("/en/kombstra/api/kombstra_polygon/?long=" + long + "&lat=" + lat)
+    .then((res) => res.json())
+    .then((data) => {
+      popup_cell_source.addFeatures(
+        new GeoJSON().readFeatures(data[0].geometry, {
+          dataProjection: "SR-ORG:97019",
+          featureProjection: "SR-ORG:97019",
+        }));
+      popupAppInst.update_popup_data(data[0].grid_id, lat, long);
+      set_overlay_position();
+    })
+    .catch((err) => {
+      console.log(err);
+      popupAppInst.set_error_msg("We are sorry, there was an error finding the data for this location.");
+      set_overlay_position_error(long, lat);
+    });
 }
 
 function remove_popup_cell_layer() {
-    popup_cell_source.clear();
+  popup_cell_source.clear();
 }
 
 // create popup and add to map
 export function create_popup() {
-    // pointermove handler to remove the popup when focus outside map
-    let map_view = map.getView();
-    function dragging_listener(evt){
-        let cell_extent = getCenter(popup_cell_source.getExtent());
-        let view_extent = map_view.getViewStateAndExtent().extent;
-        if (!containsCoordinate(view_extent, cell_extent)) {
-            overlay.setPosition(undefined);
-        } else {
-            set_overlay_position();
-        }
+  // pointermove handler to remove the popup when focus outside map
+  let map_view = map.getView();
+  function dragging_listener(evt) {
+    let cell_extent = getCenter(popup_cell_source.getExtent());
+    let view_extent = map_view.getViewStateAndExtent().extent;
+    if (!containsCoordinate(view_extent, cell_extent)) {
+      overlay.setPosition(undefined);
+    } else {
+      set_overlay_position();
     }
-    function add_dragging_handler() {
-        map.on('pointerdrag', dragging_listener)
-    };
-    function remove_dragging_handler() {
-        map.un('pointerdrag', dragging_listener)
-    };
+  }
+  function add_dragging_handler() {
+    map.on('pointerdrag', dragging_listener)
+  };
+  function remove_dragging_handler() {
+    map.un('pointerdrag', dragging_listener)
+  };
 
-    //  add popup to map
-    map.addOverlay(overlay);
-    map.addLayer(popup_cell_layer);
-    container.style.visibility = 'visible';
+  //  add popup to map
+  map.addOverlay(overlay);
+  map.addLayer(popup_cell_layer);
+  container.style.visibility = 'visible';
 
-    // add a click handler to hide the popup.
-    closer.onclick = () => {
-        overlay.setPosition(undefined);
-        closer.blur();
-        toggle_hover(true);
-        remove_popup_cell_layer();
-        remove_dragging_handler();
-        return false;
-    };
+  // add a click handler to hide the popup.
+  closer.onclick = () => {
+    overlay.setPosition(undefined);
+    closer.blur();
+    toggle_hover(true);
+    remove_popup_cell_layer();
+    remove_dragging_handler();
+    return false;
+  };
 
-    // Add a click handler to the map to render the popup.
-    map.on('singleclick', (evt) => {
-        toggle_hover(false);
+  // Add a click handler to the map to render the popup.
+  map.on('singleclick', (evt) => {
+    toggle_hover(false);
 
-        let coordinate = evt.coordinate;
-        let cell_features = popup_cell_source.getFeatures();
-        if (!((cell_features.length > 0) && cell_features[0].getGeometry().containsXY(...coordinate))) {
-            remove_popup_cell_layer();
-            update_kombstra_data(coordinate[0], coordinate[1]);
-        }
+    let coordinate = evt.coordinate;
+    let cell_features = popup_cell_source.getFeatures();
+    if (!((cell_features.length > 0) && cell_features[0].getGeometry().containsXY(...coordinate))) {
+      remove_popup_cell_layer();
+      update_kombstra_data(coordinate[0], coordinate[1]);
+    }
 
-        add_dragging_handler();
-    });
+    add_dragging_handler();
+  });
 }
