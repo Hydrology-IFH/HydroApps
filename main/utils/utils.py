01b093e0bf30771adc6cf1766e457e6fcbe21600
maxschmi <maxschm@yahoo.de>
2024-02-13 10:10:18 +0100

    DRY for context creator in utils

--- main/utils/utils.py
+++ main/utils/utils.py
@@ -1,15 +1,19 @@
 from ..settings import DEBUG
 
-def get_base_template(request):
+def get_active_app(request):
     first_url_part = request.META["PATH_INFO"].split("/")[1]
     if first_url_part in ("weather", "weatherDB", "weatherdb"):
-        return "weatherDB/base.html"
+        return "weatherDB"
     elif first_url_part == "asgII":
-        return "asgII/base.html"
+        return "asgII"
     elif first_url_part == "kombstra":
-        return "kombstra/base.html"
+        return "kombstra"
     else:
-        return "HydroApps/base.html"
+        return "HydroApps"
+
+def get_base_template(request):
+    active_app = get_active_app(request)
+    return f"{active_app}/base.html"
 
 def check_show_release(request):
     return DEBUG or (request.user.is_authenticated and request.user.is_tester)
@@ -26,15 +30,7 @@ def get_context_extra(request, **kwargs):
     if "app_name" in kwargs:
         app_name = kwargs["app_name"]
     else:
-        first_url_part = request.META["PATH_INFO"].split("/")[2]
-        if first_url_part in ("weather", "weatherDB", "weatherdb"):
-            app_name = "weatherDB"
-        elif first_url_part == "asgII":
-            app_name = "asgII"
-        elif first_url_part == "kombstra":
-            app_name = "kombstra"
-        else:
-            app_name = "HydroApps"
+        app_name = get_active_app(request)
     context.update({
         "base_template": f"{app_name}/base.html",
         "active_app": app_name})
