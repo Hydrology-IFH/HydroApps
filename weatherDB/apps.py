70642fa785b9e5d26c09db56e17e1e9473e0da51
maxschmi <maxschm@yahoo.de>
2024-02-14 16:31:11 +0100

    set the weatherdb djang_q task programmaticaly

---
+++ weatherDB/apps.py
@@ -0,0 +1,25 @@
+from django.apps import AppConfig
+from datetime import timedelta
+from django.utils import timezone
+
+class MyAuthConfig(AppConfig):
+    name = 'weatherDB'
+
+    def ready(self):
+        # create the my_auth schedules
+        from django_q.tasks import schedule
+        from django_q.models import Schedule
+
+        options = dict(
+            name="weatherDB: Cleanup Cache",
+            func="weatherDB.tasks.cleanup_cache",
+            schedule_type=Schedule.HOURLY,
+            repeats=-1,
+            next_run=timezone.now() + timedelta(minutes=5),
+        )
+
+        # update or create the schedule
+        if sched_ex := Schedule.objects.filter(name=options["name"]):
+            sched_ex.update(**options)
+        else:
+            schedule(**options)
---
+++ weatherDB/tasks.py
@@ -0,0 +1,10 @@
+from weatherDB.classes.weatherDB.broker import Broker
+from weatherDB.models import TSDownloads, CacheHCaptchaTest
+
+def regular_update(*args, **kwargs): # now done in crontab on fuhys011
+    Broker().update_db(*args, **kwargs)
+    TSDownloads.clean_files(max_days=0)
+
+def cleanup_cache(*args, **kwargs):
+    TSDownloads.clean_files()
+    CacheHCaptchaTest.clean()
