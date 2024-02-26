635c55cd9d057fb5017364ed32dfb5280d4837ab
maxschmi <maxschm@yahoo.de>
2024-02-26 09:59:55 +0100

    sri_bw: add polyfills for GECKO Browsers (Firefox) see GitHub Issue: https://github.com/openlayers/openlayers/issues/15579

--- front-end/src/sri-bw/main.js
+++ front-end/src/sri-bw/main.js
@@ -1,5 +1,8 @@
 import './style.css';
 
+import 'vite/modulepreload-polyfill';
+import './polyfill/imageSmoothingEnabled.js';
+
 import { createForm } from './modules/forms.js';
 import { create_map } from './modules/map.js';
 import { init_i18n } from './modules/i18n.js';
