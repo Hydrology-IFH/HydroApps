84b0d42e58818118d0f94d793c503e646bc07d6d
Max Schmit <max.schmit@hydrology.uni-freiburg.de>
2024-02-28 07:55:27 +0000

    remove weatherDB with uppercase

--- weatherDB/sitemaps.py
+++
@@ -1,7 +0,0 @@
-from main.base_sitemap import BaseSitemap
-
-class WeatherDBSitemap(BaseSitemap):
-    namespace = "weatherDB"
-
-    def items(self):
-        return ['home', "get_ts", "method", "package"]
