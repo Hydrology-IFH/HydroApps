052e63bb36ddc03bd26eebec513393e29fd7ed70
Max Schmit <maxschm@hotmail.com>
2024-03-22 14:46:20 +0100

    kombstra+sri_bw: add zoom to fullscreen ability

--- front-end/src/sri_bw/main.js
+++ front-end/src/sri_bw/main.js
@@ -6,7 +6,9 @@ import './polyfill/imageSmoothingEnabled.js';
 import { createForm } from './modules/forms.js';
 import { create_map } from './modules/map.js';
 import { init_i18n } from './modules/i18n.js';
+import { create_zoomer } from './modules/zoomer.js';
 
 init_i18n();
 createForm();
 create_map();
+create_zoomer();
