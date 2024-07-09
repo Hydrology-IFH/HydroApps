2d47172dd88c809611701a50dd0ad2951c870ddd
Max Schmit <maxschm@hotmail.com>
2024-07-09 12:10:36 +0200

    sfi_mockup: add menu items opacity and duration

--- front-end/src/sfi_mockup/components/Menu.vue
+++ front-end/src/sfi_mockup/components/Menu.vue
@@ -4,6 +4,7 @@
   import MenuPrecipitationForce from './menu/MenuPrecipitationForce.vue';
   import MenuSoilMoisture from './menu/MenuSoilMoisture.vue';
   import MenuDuration from './menu/MenuDuration.vue';
+  import MenuOpacity from './menu/MenuOpacity.vue';
 
   const fromRef = ref(null);
   const config = useConfig();
@@ -16,9 +17,11 @@
 </script>
 <template>
   <form class="form-horizontal" action="javascript:void(0);" ref="fromRef">
-    <h4>{{ $t('menu_header_map') }}</h4>
+    <h4>{{ $t('menu_header_event') }}</h4>
     <MenuSoilMoisture />
     <MenuPrecipitationForce />
     <MenuDuration />
+    <h4>{{ $t('menu_header_map') }}</h4>
+    <MenuOpacity />
   </form>
 </template>
