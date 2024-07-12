2f7fb331e63596c31adb720c1c1c79484b0fe5bf
maxschmi <maxschm@yahoo.de>
2024-07-12 13:43:42 +0200

    sfi_mockup: add error page on layer not existing

--- front-end/src/sfi_mockup/components/Map.vue
+++ front-end/src/sfi_mockup/components/Map.vue
@@ -3,10 +3,11 @@
   import { Map, MapControls } from "vue3-openlayers";
   import { getCenter } from 'ol/extent';
 
+  import MapHoverOverlay from "~~/components/MapHoverOverlay.vue";
+  import ErrorFrame from '~~/components/ErrorFrame.vue';
   import { useConfig } from '~/stores/config.js';
   import "./map/projections";
   import Basemaps from './map/Basemaps.vue';
-  import MapHoverOverlay from "~~/components/MapHoverOverlay.vue";
   import MapLegend from "./map/MapLegend.vue";
 
   const config = useConfig();
@@ -14,42 +15,56 @@
   const center = ref(getCenter(extent.value))
   const mapRef = ref(null);
   const map = ref(null);
+
   const layer = computed(() => config.layerLib.selectedLayer)
+  const layerError = computed(() => {
+    return layer.value ? layer.value.hasError : false
+  })
 
   onMounted(() => {
     config.layerLib.initMap(mapRef.value.map)
     config.layerLib.selectLayer("SFI")
-    map.value = mapRef.value.map;
+    map.value = mapRef.value?.map;
   })
 </script>
 
 <template>
-  <Map.OlMap id="map" ref="mapRef">
-    <Map.OlView
-      :center="center" zoom="13"
-      :extent="extent"
-      projection="EPSG:25832" />
+  <div class="map-container">
+    <Map.OlMap id="map" ref="mapRef">
+      <Map.OlView
+        :center="center" zoom="13"
+        :extent="extent"
+        projection="EPSG:25832" />
 
-      <Basemaps/>
-      <MapHoverOverlay
-          v-if="map != null && layer != null"
-          :map="map"
-          :layer="layer.olLayer"
-          :unit="layer.unit"
-          :decimals="layer.decimals"
-          :valueConverter="layer.valueConverter"/>
-      <MapLegend
-          v-if="map != null && layer != null"
-          :map="map"
-          :layerName="layer.name"
-          :style="layer.style"
-          :unit="layer.unit"/>
+        <Basemaps/>
+        <MapHoverOverlay
+            v-if="map != null && layer != null"
+            :map="map"
+            :layer="layer.olLayer"
+            :unit="layer.unit"
+            :decimals="layer.decimals"
+            :valueConverter="layer.valueConverter"/>
+        <MapLegend
+            v-if="map != null && layer != null"
+            :map="map"
+            :layerName="layer.name"
+            :style="layer.style"
+            :unit="layer.unit"/>
 
-    <MapControls.OlFullscreenControl />
-  </Map.OlMap>
+      <MapControls.OlFullscreenControl />
+
+    </Map.OlMap>
+    <ErrorFrame v-if="layerError" class="map-error" :header="$t('map_error_header')" :msg="$t('map_error_msg')"/>
+  </div>
+
 </template>
 
 <style>
+  .map-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
   #map {
     position: relative;
     width: 100%;
@@ -70,5 +85,27 @@
   .zoom-container.zoom-active #map{
     /* 100vh - 2 * margin+padding of fullscreeen - flowchart*/
     height: calc(100vh - 2*20px - 100px)
+  }
+  /* loading spinner */
+  @keyframes spinner {
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .ol-map-loading:after {
+    content: "";
+    box-sizing: border-box;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 40px;
+    height: 40px;
+    margin-top: -20px;
+    margin-left: -20px;
+    border-radius: 50%;
+    border: 5px solid rgba(180, 180, 180, 0.6);
+    border-top-color: rgba(0, 0, 0, 0.6);
+    animation: spinner 0.6s linear infinite;
   }
 </style>
