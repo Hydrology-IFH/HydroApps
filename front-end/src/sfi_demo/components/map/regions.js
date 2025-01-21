e10a7646ff3e4e62bd547d2d781a6ae08854c5d6
maxschmi <maxschm@yahoo.de>
2025-01-21 14:21:59 +0100

    sfi_demo: add Emmendingen

--- front-end/src/sfi_demo/components/map/regions.js
+++ front-end/src/sfi_demo/components/map/regions.js
@@ -2,11 +2,14 @@ import { getCenter } from 'ol/extent';
 
 export const regions = {
   Bonndorf: {
-    extent: [441576.5, 5290318.5, 456351.5, 5300468.5],
+    extent: [441576.5, 5290318.5, 456351.5, 5300468.5]
   },
   Wieslauf: {
     extent: [536286.5, 5406178.5, 545986.5, 5420628.5]
-  }
+  },
+  Emmendingen: {
+    extent: [411246.5, 5323658.5, 421616.5, 5336628.5]
+  },
 }
 
 // Add center to regions
