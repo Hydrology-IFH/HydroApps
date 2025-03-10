1422293b42128d40e0cddca83ced86f37c6e07f2
maxschmi <maxschm@yahoo.de>
2025-03-10 12:00:54 +0100

    sfi_demo: partialy release

--- sfi_demo/views.py
+++ sfi_demo/views.py
@@ -1,16 +1,12 @@
 # from django.http import HttpResponse # static http page
 from django.shortcuts import render
-from main.decorators import unreleased
 
 # Create your views here.
-@unreleased
 def home_view(request, *args, **kwargs):
     return render(request, "sfi_demo/home.html", {})
 
-@unreleased
 def app_view(request, *args, **kwargs):
     return render(request, "sfi_demo/app.html", {})
 
-@unreleased
 def method_view(request, *args, **kwargs):
     return render(request, "sfi_demo/method.html", {})
