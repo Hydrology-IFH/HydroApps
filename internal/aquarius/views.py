7989ad311aefab97835ae603af3bdad9621b70dc
Max Schmit <maxschm@hotmail.com>
2025-07-15 20:32:50 +0200

    internal.aquarius: hide app if no read permission

--- internal/aquarius/views.py
+++ internal/aquarius/views.py
@@ -2,9 +2,12 @@
 from django.shortcuts import render
 
 from main.decorators import unreleased
-# from .models import Locations
+from .config import AQUARIUS_PERMISSION_APP, PERMISSION_CLASS_EDIT, PERMISSION_CLASS_READ
 
 # Create your views here.
 @unreleased
 def aquarius_view(request, *args, **kwargs):
-    return render(request, "internal/aquarius/aquarius.html")
+    return render(request, "internal/aquarius/aquarius.html", {
+        "permission_edit": request.user.has_perm(f'{AQUARIUS_PERMISSION_APP}.{PERMISSION_CLASS_EDIT}'),
+        "permission_read": request.user.has_perm(f'{AQUARIUS_PERMISSION_APP}.{PERMISSION_CLASS_READ}'),
+    })
