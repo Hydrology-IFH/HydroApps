0b184b75bb66c26acea6bae7e1cbdcef12df8ac4
Max Schmit <maxschm@yahoo.de>
2025-11-10 15:13:56 +0100

    frontend/common: make selection list scrolobale with arrows

--- front-end/src/common/components/inputs/SelectionInput.vue
+++ front-end/src/common/components/inputs/SelectionInput.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { computed } from 'vue';
+  import { computed, ref, onMounted } from 'vue';
 
   import BaseInput from './BaseInput.vue';
 
@@ -25,7 +25,7 @@
     type: [String, Number],
     default: () => []
   });
-  defineEmits(['change']);
+  const emits = defineEmits(['change']);
 
   const optionsCleaned = computed(() => {
     if (Array.isArray(props.options)) {
@@ -46,12 +46,67 @@
     return [];
   });
 
+  // add arrow listener
+  const selectRef = ref(null);
+  const buttonsRef = ref({});
+  const handleKeyDown = (event) => {
+      if (event.key === 'ArrowRight' || event.key === 'ArrowLeft' || event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+        event.preventDefault();
+        event.stopPropagation();
+        let currentIndex = optionsCleaned.value.map(option => option.key).indexOf(model.value);
+        let nextIndex;
+        console.log("asButtons:", props.asButtons, "currentIndex:", currentIndex);
+        if (props.asButtons) {
+          if (event.key === 'ArrowRight') {
+            nextIndex = (currentIndex + 1) % optionsCleaned.value.length;
+          } else if (event.key === 'ArrowLeft') {
+            nextIndex = (currentIndex - 1 + optionsCleaned.value.length) % optionsCleaned.value.length;
+          }
+        } else {
+          if (event.key === 'ArrowDown') {
+            nextIndex = (currentIndex + 1) % optionsCleaned.value.length;
+          } else if (event.key === 'ArrowUp') {
+            nextIndex = (currentIndex - 1 + optionsCleaned.value.length) % optionsCleaned.value.length;
+          }
+        }
+        if (!nextIndex) {
+          console.warn("Unhandled arrow key:", event.key);
+          return;
+        }
+        model.value = optionsCleaned.value[nextIndex].key;
+        emits('change', optionsCleaned.value[nextIndex].key);
+        event.preventDefault();
+        event.stopPropagation();
+      }
+    };
+  const addEventListeners = (refObj) => {
+    refObj.value.addEventListener('focus', () => {
+      window.addEventListener('keydown', handleKeyDown);
+      refObj.value.addEventListener('focusout', () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      });
+    });
+  };
+
+  onMounted(() => {
+    window.selectRef = selectRef;
+    if (props.asButtons) {
+      Object.keys(buttonsRef.value).forEach((key) => {
+        addEventListeners(buttonsRef.value[key]);
+      });
+    } else {
+      addEventListeners(selectRef);
+    }
+  });
+
+
 </script>
 
 <template>
   <BaseInput :label="label">
     <select
       v-if="!asButtons"
+      ref="selectRef"
       v-model="model"
       class="form-select"
       :aria-label="label"
@@ -72,6 +127,7 @@
     >
       <button
         v-for="option in optionsCleaned"
+        ref="buttonsRef[option.key]"
         :key="option.key"
         class="btn btn-stretch"
         :class="[(option.key == model) ? 'btn-primary':'btn-secondary']"
