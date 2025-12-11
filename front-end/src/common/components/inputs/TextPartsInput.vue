5394511360ca9327f0b904919ba0576b1ba0dce1
Max Schmit <maxschm@hotmail.com>
2025-12-11 13:21:38 +0100

    fronten/comon: make TextParts nicer

--- front-end/src/common/components/inputs/TextPartsInput.vue
+++ front-end/src/common/components/inputs/TextPartsInput.vue
@@ -1,36 +1,79 @@
 <script setup>
+  import { computed } from 'vue'
+
   import BaseInput from './BaseInput.vue'
+  import TextPartInput from './utils/TextPartInput.vue'
+
   const model = defineModel({ type: Array[String], required: true })
   const props = defineProps({
     disabled: { type: Boolean, default: false }
+  })
+
+  const firstPart = computed({
+    get: () => model.value[0] ?? '',
+    set: (value) => {
+      if (model.value.length === 0) {
+        if (value !== '') model.value.push(value);
+      } else {
+        if (value === '') {
+          model.value.shift()
+        } else {
+         model.value[0] = value
+        }
+      }
+    }
   })
 
 </script>
 
 <template>
   <BaseInput>
-    <input
-      v-for="(textPart, index) in model"
-      :key="index"
-      v-model="model[index]"
-      class="form-control"
-      type="text"
-      :disabled="props.disabled"
-    >
-    <input
-      v-if="model.length === 0"
-      v-model="model[0]"
-      class="form-control"
-      type="text"
-      :disabled="props.disabled"
-    >
-    <button
-      v-if="!props.disabled"
-      type="button"
-      class="btn btn-secondary mt-2"
-      @click="model.push('')"
-    >
-      <i class="btn bi-plus" />
-    </button>
+    <div class="form-control text-part-input-container">
+      <TextPartInput
+        :key="0"
+        v-model="firstPart"
+        :add-adder-button="(!props.disabled) && (model.length > 0)"
+        :add-remover-button="(!props.disabled) && (model.length > 0)"
+        @add-part="model.splice(1, 0, '')"
+        @remove-part="model.shift()"
+      />
+      <template
+        v-if="model.length > 1">
+        <TextPartInput
+          v-for="(textPart, index) in model.slice(1)"
+          :key="index"
+          v-model="model[index+1]"
+          :add-remover-button="!props.disabled"
+          :add-adder-button="!props.disabled"
+          @add-part="model.splice(index + 2, 0, '')"
+          @remove-part="model.splice(index + 1, 1)"
+        />
+      </template>
+      <!--  -->
+    </div>
   </BaseInput>
 </template>
+
+<style>
+  .text-part-input-container{
+    margin: 0;
+    padding: 0;
+  }
+  .text-part-input-container > .input-group{
+    border: none;
+    box-shadow: none;
+    padding: 0;
+    margin: -1px;
+    width: calc(100% + 2px);
+    border-radius: 0;
+  }
+  .text-part-input-container > .input-group > * {
+    border-radius: 0;
+  }
+  .text-part-input-container > .input-group:first-of-type > :last-child{
+    border-top-right-radius: var(--bs-border-radius) ;
+  }
+  .text-part-input-container > .input-group:last-of-type > :last-child{
+    border-bottom-right-radius: var(--bs-border-radius) ;
+  }
+</style>
---
+++ front-end/src/common/components/inputs/utils/TextPartInput.vue
@@ -0,0 +1,48 @@
+<script setup>
+  const model = defineModel({ type: String, required: true })
+  const props = defineProps({
+    addRemoverButton: { type: Boolean },
+    addAdderButton: { type: Boolean }
+  })
+  const emits = defineEmits(['addPart', "removePart"])
+
+</script>
+
+<template>
+  <div class="input-group">
+    <input
+      v-model="model"
+      class="form-control"
+      type="text"
+      :disabled="props.disabled"
+      data-bs-toggle="tooltip"
+      data-bs-placement="top"
+      :data-bs-title="$t('common:text_part_input-remove_tooltip')"
+    >
+    <button
+      v-if="props.addRemoverButton"
+      type="button"
+      class="btn btn-outline-secondary"
+      data-bs-toggle="tooltip"
+      data-bs-placement="top"
+      :data-bs-title="$t('common:text_part_input-add_tooltip')"
+      @click="emits('removePart')"
+    >
+      <i class="btn bi-trash" />
+    </button>
+    <button
+      v-if="props.addAdderButton"
+      type="button"
+      class="btn btn-outline-secondary"
+      @click="emits('addPart')"
+    >
+      <i class="btn bi-plus" />
+    </button>
+  </div>
+</template>
+
+<style scoped>
+  button.btn {
+    padding: 0;
+  }
+</style>
