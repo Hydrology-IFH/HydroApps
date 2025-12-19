2ba2bafcd8d7b5bea8d9270604d7779aa30b143f
Max Schmit <maxschm@hotmail.com>
2025-12-19 02:07:21 +0100

    internal/aquarius: fix view permission edit also on token

--- internal/aquarius/views.py
+++ internal/aquarius/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 
 from my_auth.decorators import check_permissions
 from my_auth.config import USER_CLASS, EDIT_USER_CLASS
+from my_auth.models import TokenPermission
 from .config import AQUARIUS_APP_NAME
 from ..config import APP_NAME as INTERNAL_APP_NAME
 from ..context_processor import internal_context_processor
@@ -12,7 +13,12 @@ from ..context_processor import internal_context_processor
     f'{INTERNAL_APP_NAME}.{USER_CLASS}',
     f'{AQUARIUS_APP_NAME}.{USER_CLASS}'])
 def aquarius_view(request, *args, **kwargs):
-    return render(request, "internal/aquarius/aquarius.html", {
-        "permission_edit": request.user.has_perm(f'{AQUARIUS_APP_NAME}.{EDIT_USER_CLASS}'),
-        **internal_context_processor(request)
-    })
+    context = internal_context_processor(request)
+    token = request.session.get("token_permission", None)
+    if token:
+        if TokenPermission.objects.get(token=token)\
+                .has_perm(f"{AQUARIUS_APP_NAME}.{EDIT_USER_CLASS}"):
+            context["permission_edit"] = True
+    if "permission_edit" not in context:
+        context["permission_edit"] = request.user.has_perm(f'{AQUARIUS_APP_NAME}.{EDIT_USER_CLASS}')
+    return render(request, "internal/aquarius/aquarius.html", context)
